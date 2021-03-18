f709840f58eceb779bdff02d4392046f7faa61f5
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-17 20:52:35 -0400

    project set up

---
+++ image_detection/image_detection/image_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_detectionApp.swift
+//  image_detection
+//
+//  Created by X Y on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct image_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
